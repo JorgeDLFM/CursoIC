0b0763ccf3ea64d23bfefaaae16b0afd1dd03974
JorgeDLFM <jfuentemartin@gmail.com>
2024-09-18 13:03:33 +0200

    Add files via upload

---
+++ src/main/java/com/curso/Indra_Artifact/IndraArtifactApplication.java
@@ -0,0 +1,13 @@
+package com.curso.Indra_Artifact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndraArtifactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndraArtifactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/curso/Indra_Artifact/IndraArtifactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.Indra_Artifact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndraArtifactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
