7ac2fdfd4c0014dc025016e6c61fabde63a72d40
JorgeDLFM <jfuentemartin@gmail.com>
2024-09-19 10:22:55 +0200

    Add files via upload

---
+++ Indra_Artifact_7/src/main/java/com/example/Indra_Artifact_7/IndraArtifact7Application.java
@@ -0,0 +1,13 @@
+package com.example.Indra_Artifact_7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndraArtifact7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndraArtifact7Application.class, args);
+	}
+
+}
---
+++ Indra_Artifact_7/src/test/java/com/example/Indra_Artifact_7/IndraArtifact7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Indra_Artifact_7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndraArtifact7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
