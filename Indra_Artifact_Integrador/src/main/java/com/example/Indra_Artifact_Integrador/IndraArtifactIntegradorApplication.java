81594fd850eb471d750da290974e9f1db38afb8a
JorgeDLFM <jfuentemartin@gmail.com>
2024-09-20 12:04:01 +0200

    Add files via upload

---
+++ Indra_Artifact_Integrador/src/main/java/com/example/Indra_Artifact_Integrador/IndraArtifactIntegradorApplication.java
@@ -0,0 +1,13 @@
+package com.example.Indra_Artifact_Integrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndraArtifactIntegradorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndraArtifactIntegradorApplication.class, args);
+	}
+
+}
---
+++ Indra_Artifact_Integrador/src/test/java/com/example/Indra_Artifact_Integrador/IndraArtifactIntegradorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Indra_Artifact_Integrador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndraArtifactIntegradorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
