72b2f3ef7233c6d7b090219f5c04d8f4d65be721
JorgeDLFM <jfuentemartin@gmail.com>
2024-09-23 10:23:21 +0200

    Add files via upload

---
+++ Indra_Artifact_Examen/src/main/java/com/example/Indra_Artifact_Examen/IndraArtifactExamenApplication.java
@@ -0,0 +1,13 @@
+package com.example.Indra_Artifact_Examen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndraArtifactExamenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndraArtifactExamenApplication.class, args);
+	}
+
+}
---
+++ Indra_Artifact_Examen/src/test/java/com/example/Indra_Artifact_Examen/IndraArtifactExamenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Indra_Artifact_Examen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndraArtifactExamenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
